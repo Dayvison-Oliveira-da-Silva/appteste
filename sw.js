719f24a69185a61735cca05bf76cd4b9cc5f3721
Dayvison-Oliveira-da-Silva <dayvison.shelf@gmail.com>
2025-11-18 15:45:26 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'shelfpass-cache-v1';
+const ASSETS = [
+  '/appteste/',
+  '/appteste/index.html',
+  '/appteste/manifest.json'
+  // depois você adiciona aqui CSS, imagens, JS, etc
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
